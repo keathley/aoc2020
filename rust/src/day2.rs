e235e35ad0f1e4556685dbf6ef08ed32c0a24820
Chris Keathley <c@keathley.io>
2020-12-02 13:00:51 -0500

    Remove nonsense

--- rust/src/day2.rs
+++ rust/src/day2.rs
@@ -9,77 +9,39 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-enum Policy {
-    Old,
-    New
-}
-
-#[derive(Debug)]
-struct Line {
-    index1: usize,
-    index2: usize,
-    target_char: char,
-    password: String,
-}
-
-impl Line {
-    fn is_valid(&self, policy: Policy) -> bool {
-        match policy {
-            Policy::Old => {
-                let mut count = 0;
-                for c in self.password.chars() {
-                    if c == self.target_char {
-                        count += 1;
-                    }
-                }
-                self.index1 <= count && count <= self.index2
-            }
-
-            Policy::New => {
-                let first  = self.password.chars().nth(self.index1-1);
-                let second = self.password.chars().nth(self.index2-1);
-
-                match (first, second) {
-                    (Some(first), Some(second)) => {
-                        return xor(first == self.target_char, second == self.target_char)
-                    }
-                    _ => {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-}
-
-fn xor(a: bool, b: bool) -> bool {
-    (a && !b) || (b && !a)
-}
-
 pub fn solve() {
     let re = Regex::new(r"(\d+)-(\d+) ([a-z]): (.*)$").unwrap();
 
-    let lines: Vec<Line> = read_lines("data/day2.txt").unwrap()
+    let lines = read_lines("data/day2.txt").unwrap()
         .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+
+    let input: Vec<(usize, usize, char, &str)> = lines.iter()
         .map(|line| {
             let captures = re.captures(&line).unwrap();
-            Line{
-                index1: captures.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-                index2: captures.get(2).unwrap().as_str().parse::<usize>().unwrap(),
-                target_char: captures.get(3).unwrap().as_str().to_string().chars().nth(0).unwrap(),
-                password: captures.get(4).unwrap().as_str().to_string()
-            }
+            let index1 = captures.get(1).unwrap().as_str().parse().unwrap();
+            let index2 = captures.get(2).unwrap().as_str().parse().unwrap();
+            let target = captures.get(3).unwrap().as_str().chars().next().unwrap();
+            let password = captures.get(4).unwrap().as_str();
+            (index1, index2, target, password)
         })
         .collect();
 
-    let count = lines.iter()
-        .filter(|line| line.is_valid(Policy::Old))
-        .count();
-
+    let mut count = 0;
+    for &(i, j, c, pass) in &input {
+        let char_count = pass.matches(c).count();
+        if i <= char_count && char_count <= j {
+            count += 1;
+        }
+    }
     println!("Part 1: {}", count);
 
-    let count = lines.iter()
-        .filter(|line| line.is_valid(Policy::New))
-        .count();
+    let mut count = 0;
+    for &(i, j, c, pass) in &input {
+        let chars: Vec<char> = pass.chars().collect();
+        if (chars[i-1] == c) != (chars[j-1] == c) {
+            count += 1;
+        }
+    }
     println!("Part 2: {}", count);
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,9 +4,10 @@ mod day1;
 mod day2;
 
 fn main() {
+    println!("\nDay 1");
     println!("Part 1: {}", day1::part1());
     println!("Part 2: {}", day1::part2());
 
-    println!("Day 2");
+    println!("\nDay 2");
     day2::solve();
 }
