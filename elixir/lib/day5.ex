6eb62a7dd75ea3af169b537a2308af4428819707
Chris Keathley <c@keathley.io>
2020-12-05 13:11:36 -0500

    Day 5 solution

--- elixir/lib/day5.ex
+++ elixir/lib/day5.ex
@@ -1,26 +1,42 @@
 defmodule Day5 do
-  def solve do
+  @input File.read!(Path.absname("priv/day5.txt", Application.app_dir(:aoc2020)))
 
+  def solve do
+    ids =
+      @input
+      |> String.split()
+      |> IO.inspect(label: "Split")
+      |> Enum.map(&to_id/1)
+
+    part1 = Enum.max(ids)
+    IO.puts "Part 1: #{part1}"
+
+    start    = Enum.min(ids)
+    stop     = Enum.max(ids)
+    expected = MapSet.new(start..stop)
+    actual   = MapSet.new(ids)
+    part2    = MapSet.difference(expected, actual)
+    IO.puts "Part 2: #{Enum.at(Enum.to_list(part2), 0)}"
   end
 
   def to_id(str) do
     <<row :: binary-size(7), col :: binary-size(3)>> = str
-    # row    = String.slice(str, 0..6)
-    # column = String.slice(str, 7..9)
+    row = for char <- String.graphemes(row), do: (if char == "F", do: :lower, else: :upper)
+    col = for char <- String.graphemes(col), do: (if char == "L", do: :lower, else: :upper)
 
-    row = find_row(row, 0, 127)
-    # column = find_column(col)
+    row = find_spot(row, 0, 127)
+    col = find_spot(col, 0, 7)
 
-    # row * 8 + column
+    row * 8 + col
   end
 
-  def find_row(str, start, stop) do
+  def find_spot(moves, start, stop) do
     midpoint = div(start + (stop-1), 2)
 
-    case str do
-      <<"F", rest :: binary>> -> find_row(rest, start, midpoint)
-      <<"B", rest :: binary>> -> find_row(rest, midpoint+1, stop)
-      <<>> -> start
+    case moves do
+      [:lower | rest] -> find_spot(rest, start, midpoint)
+      [:upper | rest] -> find_spot(rest, midpoint+1, stop)
+      [] -> start
     end
   end
 end
