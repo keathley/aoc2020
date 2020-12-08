045e4f837089cd92a0a687a3ea5881668c38c238
Chris Keathley <c@keathley.io>
2020-12-08 09:42:16 -0500

    Day8 part 2

--- elixir/lib/day8.ex
+++ elixir/lib/day8.ex
@@ -1,18 +1,39 @@
 defmodule Day8 do
   @input File.read!(Path.absname("priv/day8.txt", Application.app_dir(:aoc2020)))
-
   import Enum, only: [at: 2]
 
   def solve(input \\ @input) do
-    ops = parse(input)
-    state = %{ops: ops, eip: 0, acc: 0, seen: MapSet.new()}
-    interpret(state)
+    ops   = parse(input)
+    state = %{
+      parent: self(),
+      ops: ops,
+      eip: 0,
+      acc: 0,
+      seen: MapSet.new(),
+      exit_loc: Enum.count(ops),
+    }
+    t = Task.async(fn -> interpret(state) end)
+    {:infinite, result} = Task.await(t)
+    IO.puts "Part 1: #{result}"
+
+    result =
+      state
+      |> mutate_instructions()
+      |> Enum.map(fn m -> Task.async(fn -> interpret(m) end) end)
+      |> Enum.map(&Task.await/1)
+      |> Enum.find_value(fn
+        {:done, acc}      -> acc
+        {:infinite, _acc} -> false
+      end)
+    IO.puts "Part 2: #{result}"
   end
 
+  def interpret(%{eip: eip, exit_loc: eip, acc: acc}) do
+    {:done, acc}
+  end
   def interpret(%{ops: ops, eip: eip, acc: acc}=state) do
     if MapSet.member?(state.seen, eip) do
-      IO.puts "Beginning Loop: #{acc}"
-      throw :exit
+      throw :infinite
     end
 
     state = update_in(state, [:seen], & MapSet.put(&1, eip))
@@ -28,6 +49,29 @@ defmodule Day8 do
       {:jmp, val} ->
         interpret(%{state | eip: eip+val})
     end
+  catch
+    :infinite ->
+      {:infinite, acc}
+  end
+
+  defp mutate_instructions(%{ops: ops}=state) do
+    mutations = for i <- 0..Enum.count(ops)-1 do
+      case at(ops, i) do
+        {:nop, _} -> %{state | ops: flip(ops, i)}
+        {:jmp, _} -> %{state | ops: flip(ops, i)}
+        _         -> :skip
+      end
+    end
+    Enum.reject(mutations, & &1 == :skip)
+  end
+
+  defp flip(ops, i) do
+    update_in(ops, [Access.at(i)], fn op ->
+      case op do
+        {:nop, val} -> {:jmp, val}
+        {:jmp, val} -> {:nop, val}
+      end
+    end)
   end
 
   def parse(str \\ @input) do
